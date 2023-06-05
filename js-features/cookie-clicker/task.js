0fe6ea0d8f9a845e1788c0f0eb98d0934886eef1
Vladislav B <fashionbeatt@gmail.com>
2023-06-05 21:16:09 +0300

    fix 1st tasks "cookies"

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,15 +1,15 @@
 const cookie = document.getElementById("cookie");
-const clickerCounter = document.getElementById("clicker__counter");
-const clickerSpeed = document.getElementById("clicker__speed");
-let value = 0;
 
-cookie.onclick = function() {
-    value++;
-    clickerCounter.textContent = value;
-    if (value % 2 == 0) {
-        cookie.width = 250;
-    } else {
-        cookie.width = 180;
-    }
+const clicker__counter = document.getElementById("clicker__counter");
+let count = 0;
 
-}
+cookie.onclick = function () {
+  if (count % 2 === 0) {
+    cookie.width = 100;
+  } else {
+    cookie.width = 200;
+  }
+  
+  count++;
+  clicker__counter.textContent = count;
+ };
